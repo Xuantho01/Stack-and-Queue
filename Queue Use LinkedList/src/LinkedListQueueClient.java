6c326887700a38d635b912181dd60494cad4200c
Xuantho01 <hoxuantho95@gmail.com>
2020-02-27 16:27:16 +0700

    queue us linkedList

---
+++ Queue Use LinkedList/src/LinkedListQueueClient.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class LinkedListQueueClient {
+    public static void main(String[] args) {
+        MyLinkedListQueue queue = new MyLinkedListQueue();
+        queue.enqueue(10);
+        queue.enqueue(20);
+
+        System.out.println(queue.dequeue().key);
+    }
+}
